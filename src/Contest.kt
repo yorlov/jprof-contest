d8b95f09341879bce626a81f2149d9a5740a68ed
Yuri Orlov <yorlov@stiltsoft.com>
2017-12-31 11:25:43 +0300

    Updated use of the reader

--- src/Contest.kt
+++ src/Contest.kt
@@ -1,40 +1,31 @@
-import java.io.BufferedReader
 import java.net.URL
+import java.util.regex.Matcher
 import java.util.regex.Pattern
 
 
 fun main(args: Array<String>) {
-    val pattern = Pattern.compile("\"download_url\":\"(.*?)\"")
-
     val urls = hashSetOf(
             "https://raw.githubusercontent.com/JavaBy/jprof-hugo/master/content/about.md",
             "https://raw.githubusercontent.com/JavaBy/jprof-hugo/master/content/contact.md")
 
-    reader("https://api.github.com/repos/JavaBy/jprof-hugo/contents/content/post") {
-        val matcher = pattern.matcher(readLine())
-        while (matcher.find()) {
-            urls.add(matcher.group(1))
-        }
+    val download = Pattern.compile("\"download_url\":\"(.*?)\"")
+
+    forEachMatchedLine("https://api.github.com/repos/JavaBy/jprof-hugo/contents/content/post", download) { matcher ->
+        urls.add(matcher.group(1))
     }
 
     val word = Pattern.compile("java", Pattern.CASE_INSENSITIVE)
 
     val total = urls.stream().map { url ->
         var count = 0
-        reader(url) {
-            var line = readLine()
-            while (line != null) {
-                val matcher = word.matcher(line)
-                while (matcher.find()) {
-                    count++
-                }
-                line = readLine()
-            }
-        }
+        forEachMatchedLine(url, word) { count++ }
         count
     }.parallel().reduce(0, Integer::sum)
 
     println(total)
 }
 
-fun reader(url: String, block: BufferedReader.() -> Unit) = URL(url).openConnection().inputStream.bufferedReader().use(block)
+fun forEachMatchedLine(url: String, pattern: Pattern, block: (Matcher) -> Unit) = URL(url).openConnection().inputStream.reader().forEachLine {
+    val matcher = pattern.matcher(it)
+    while (matcher.find()) block(matcher)
+}
